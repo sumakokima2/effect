e4701b9fad68817dfb93be25ea3eeca84cb5e0ad
原田真喜子 <makikoharada@haradamakiko-no-MacBook-Air-2.local>
2020-06-09 14:46:12 +0900

    test

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React,{useState,useEffect} from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return( <Lifecyclecheck /> );
+}
+
+const Lifecyclecheck = () =>{
+    const [Checkstop,setCheckstop] = useState(true);
+    const [Checktext,setChecktext] = useState("stop");
+
+    const stop = () => {
+        console.log(Checkstop);
+        Checkstop ? setCheckstop(false) : setCheckstop(true);
+        Checkstop ? setChecktext("go") : setChecktext("stop");
+    }
+    if(Checkstop){
+        return <div><Lifecycle stop={Checkstop} text = {Checktext}/><button onClick={stop}>{Checktext}</button></div>;
+    }
+    else{
+        return <div><Lifecycle stop={Checkstop} text = {Checktext}/><button onClick={stop}>{Checktext}</button></div>;
+    }
+}
+
+const Lifecycle = (props) =>{
+    const [today,setToday] = useState(new Date().toLocaleTimeString());
+    
+    const tick= () =>{
+        console.log(props.stop);
+        setToday(new Date().toLocaleTimeString());
+    }
+    
+    useEffect(() => {
+        if(props.stop){
+            const timer = setInterval(() => {
+              tick()
+            }, 1000);
+            return () => clearInterval(timer);
+        }
+    }, [props.stop]);
+
+    return <div>{today}</div>;
+}
+ReactDom.render(
+    <App />,
+    document.querySelector("#root")
+);
